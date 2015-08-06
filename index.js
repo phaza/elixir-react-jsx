5147d8c014495d94aee9a1b64cfd32e6a8b22b1d
Peter Haza <peter.haza@gmail.com>
2015-08-06 15:45:17 +0200

    Update index.js
    Support for elixir 3.0.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var elixir = require('laravel-elixir');
-var gulp = require('gulp');
-var gulpif = require('gulp-if');
-var react = require('gulp-react');
-var concat = require('gulp-concat');
+var Elixir = require( 'laravel-elixir' ),
+    gulp   = require( 'gulp' ),
+    gulpif = require( 'gulp-if' ),
+    react  = require( 'gulp-react' ),
+    concat = require( 'gulp-concat' ),
+    Task   = Elixir.Task;
 
-elixir.extend("jsx", function(src, dest) {
-    src = src || 'resources/assets/jsx/*.jsx';
-    dest = dest || 'public/js';
+Elixir.extend( "jsx", function ( src, dest )
+{
+	src  = src || 'resources/assets/jsx/*.jsx';
+	dest = dest || 'public/js';
 
-    var doConcat = ~dest.indexOf('.js');
+	var doConcat = ~dest.indexOf( '.js' );
 
-    gulp.task("jsx", function() {
-        gulp.src(src)
-            .pipe(react())
-            .pipe(gulpif(doConcat, concat(dest)))
-            .pipe(gulp.dest(dest));
-    });
 
-    return this.queueTask("jsx");
-});
+	new Task('jsx', function() {
+		gulp.src( src )
+			.pipe( react() )
+			.pipe( gulpif( doConcat, concat( dest ) ) )
+			.pipe( gulp.dest( dest ) );
+	}).watch(src);
+} );
